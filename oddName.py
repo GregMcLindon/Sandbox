9e82fd69196a1a98d66c5c1b81ac067bffef360a
GregMcLindon <gregory.mclindon@my.jcu.edu.au>
2017-08-13 13:12:29 +1000

    Add name printing program

--- oddName.py
+++ oddName.py
@@ -1,1 +1,12 @@
 """Greg McLindon"""
+def main():
+    """Program to print every second letter of a string"""
+name_valid = False
+string_name = ""
+while not name_valid:
+    string_name = input("What is your name?")
+    if len(string_name) == 0:
+        print("Enter a valid name")
+    else:
+        name_valid = True
+print(string_name[::2])
