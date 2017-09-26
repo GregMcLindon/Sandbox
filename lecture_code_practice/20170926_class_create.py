cc5e7d1a1577e9f3e0f98208bd6a29810b8d6f26
GregMcLindon <gregory.mclindon@my.jcu.edu.au>
2017-09-26 21:39:24 +1000

    add percentage discount

--- lecture_code_practice/20170926_class_create.py
+++ lecture_code_practice/20170926_class_create.py
@@ -13,6 +13,17 @@ class MountainBike:
         return "{} by {} price: ${:.2f}{}".format(
             self.model, self.brand, self.price, " (On Sale!)" if self.is_on_sale else "")
 
+    def put_on_sale(self, percentage):
+        """
+        Put product on sale reducing price by passed in percentage
+        :param percentage: like 15 for 15%
+        :return: None
+        """
+        self.price -= self.price * percentage / 100
+
+
 print(MountainBike())
 bike_test = MountainBike('Trek', 'Fuel', 4799, True)
+print(bike_test)
+bike_test.put_on_sale(10)
 print(bike_test)
